d921fca01d672994d336854d0a048a474e5d91e9
Yannick Fricke <yannickfricke@googlemail.com>
2019-07-05 18:44:04 +0200

    Added the GAListener

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import About from './about/About';
 import Contact from './contact/Contact';
@@ -9,16 +10,40 @@ import { ABOUTME_URL, CONTACTME_URL, FRONTPAGE_URL } from './routes';
 
 import './App.scss';
 
+class GAListener extends React.Component {
+    public componentDidMount() {
+        ReactGA.initialize('UA-143361335-1');
+        this.sendPageView();
+        window.addEventListener('hashchange', () => {
+            this.sendPageView();
+        });
+    }
+
+    public sendPageView() {
+        const page = location.hash.substr(1);
+
+        ReactGA.set({ page });
+        ReactGA.pageview(page);
+    }
+
+    // tslint:disable-next-line:no-any
+    public render() {
+        return this.props.children;
+    }
+}
+
 export const App: React.FC = () => {
     return <HashRouter>
-        <Menu />
-        <div className="container">
-            <Switch>
-                <Route exact path={FRONTPAGE_URL} component={Frontpage} />
-                <Route path={ABOUTME_URL} component={About} />
-                <Route path={CONTACTME_URL} component={Contact} />
-                <Route path={'/post/:name'} component={PostRoute} />
-            </Switch>
-        </div>
+        <GAListener>
+            <Menu />
+            <div className="container">
+                <Switch>
+                    <Route exact path={FRONTPAGE_URL} component={Frontpage} />
+                    <Route path={ABOUTME_URL} component={About} />
+                    <Route path={CONTACTME_URL} component={Contact} />
+                    <Route path={'/post/:name'} component={PostRoute} />
+                </Switch>
+            </div>
+        </GAListener>
     </HashRouter>;
 };
