927b5cb32bf1581a0807d57b4e0aa74981a769f5
Yannick Fricke <yannickfricke@googlemail.com>
2019-07-06 22:17:23 +0200

    Use own formatter function for the date

--- src/posts/Post.tsx
+++ src/posts/Post.tsx
@@ -1,4 +1,3 @@
-import moment = require('moment');
 import React from 'react';
 import { IPost } from './IPost';
 
@@ -6,12 +5,19 @@ interface IPostProps {
     post: IPost;
 }
 
+export const formatDate = (date: Date): string => {
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth()}`.padStart(2, '0');
+
+    return `${day}.${month}.${date.getFullYear()}`;
+};
+
 export const Post: React.FC<IPostProps> = (props) => {
     const PostComponent = props.post.Component;
 
     return <div>
         <h1>{props.post.title}</h1>
-        <p className={'publishdate'}>Published at {moment(props.post.publishDate).format('DD.MM.YYYY')}</p>
+        <p className={'publishdate'}>Published at {formatDate(props.post.publishDate)}</p>
         <PostComponent />
     </div>;
 };
