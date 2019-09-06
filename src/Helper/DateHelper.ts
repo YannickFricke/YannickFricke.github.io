32a264fcaada3768521b7b0591402234daa8a622
Yannick Fricke <yannickfricke@googlemail.com>
2019-09-06 15:31:30 +0200

    Formatted the Datehelper file

--- src/Helper/DateHelper.ts
+++ src/Helper/DateHelper.ts
@@ -1,11 +1,11 @@
-export const FormatDate = (date: Date) => {
-    return `${getPrefixedNumber(date.getMonth() + 1)}.${getPrefixedNumber(date.getDay())}.${date.getFullYear()}`;
-};
-
 const getPrefixedNumber: (number: number) => string = (number: number) => {
     if (number > 9) {
         return number.toString();
     }
 
     return number.toString(10).padStart(2, '0');
+};
+
+export const FormatDate = (date: Date) => {
+    return `${getPrefixedNumber(date.getDate())}.${getPrefixedNumber(date.getMonth() + 1)}.${date.getFullYear()}`;
 };
