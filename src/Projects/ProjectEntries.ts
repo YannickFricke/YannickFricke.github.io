60cde1489b959a055c95f697ec896869f9b0e940
Yannick Fricke <yannickfricke@googlemail.com>
2019-10-20 15:25:16 +0200

    Added the Twitch unfollow notifier project

--- src/Projects/ProjectEntries.ts
+++ src/Projects/ProjectEntries.ts
@@ -7,6 +7,18 @@ export interface IProjectEntry {
 
 export const ProjectEntries: IProjectEntry[] = [
     {
+        name: 'Twitch Unfollow Notifier',
+        url: 'https://github.com/YannickFricke/TwitchUnfollowNotifier',
+        usedLanguages: [
+            'Node.JS',
+            'TypeScript',
+        ],
+        gainedExpertise: [
+            'Working with the Twitch API',
+            'Working with rate limits',
+        ],
+    },
+    {
         name: 'BashMigration',
         url: 'https://github.com/YannickFricke/BashMigration',
         usedLanguages: [
