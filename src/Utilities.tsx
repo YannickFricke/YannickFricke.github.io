8b933b06b629626a0bfd266024cb22bd9c22e556
Yannick Fricke <yfr@mts.de>
2020-04-22 12:58:55 +0200

    Upgraded dependencies and fixed react-helmet import

--- src/Utilities.tsx
+++ src/Utilities.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 interface ITitleProps {
     title: string;
 }
 
-export const Title: React.FC<ITitleProps> = (props) => {
-    return <Helmet>
-        <title>{props.title} | Yannick Fricke</title>
-    </Helmet>;
-};
+export const Title: React.FC<ITitleProps> = (props) => (<Helmet>
+    <title>{props.title} | Yannick Fricke</title>
+</Helmet>);
